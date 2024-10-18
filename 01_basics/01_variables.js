dbcadfb60f395f14cd03bed02d852086cee8a7f4
Rishabhdev33 <158014701+Rishabhdev33@users.noreply.github.com>
2024-10-18 11:22:59 +0000

    Pending changes exported from your codespace

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "ishabhdevmakoder1610@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+let accountState;
+
+// accountId = 2 // not allowed to change the const 
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var because of issu in block scope and functional scope
+*/
