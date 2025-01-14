d3bf71f4b4769213164995dbcd2b9c0cf52c11d7
Rishabhdev33 <158014701+Rishabhdev33@users.noreply.github.com>
2025-01-14 12:57:13 +0530

    Add files via upload

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+let score = 33
+//console.log(typeof score);
+
+// Operations
+
+let value =3
+let negValue = -value
+//console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Rishabhdev"
+let str3 = str1 + str2
+//console.log(str3)
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
